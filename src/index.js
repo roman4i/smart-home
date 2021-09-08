0a7e7c3c979048ebd0788451fa7c097c75a721ed
roman4i <verb.r17@gmail.com>
2021-09-08 20:35:31 +0300

    Added 2 files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import './style.css'
 
 const heading = document.createElement('div')
 heading.textContent = 'Таки работает!'
-heading.classList = 'test'
 
 document.getElementById('root').append(heading);
