3367ded0c276ebead2773dcf121042d92784a5ab
roman4i <verb.r17@gmail.com>
2021-10-16 19:51:49 +0300

    Added simple footer logs line

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import insertHeader from './header/header';
 import menuBlock from './menus';
+import footer from './footer';
 
 const root = document.getElementById('root');
 
 root.append(insertHeader());
 root.append(menuBlock());
+root.append(footer());
 
